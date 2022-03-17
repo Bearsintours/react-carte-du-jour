4977aa8602cdebcd5d8a590ed104d3873df854b8
Quentin <qbrogly@gmail.com>
2022-03-16 21:34:56 -0700

    Move add recipe btn and search bar

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,19 +1,19 @@
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
-import SearchForm from "./SearchForm";
 import RecipeList from "./RecipeList";
 
 export const DashboardPage = () => {
   return (
     <div className="dashboard">
-      <div className="container">
-        <SearchForm />
-        <RecipeList />
+      <div className="dashboard__top">
         <Link to="/create">
           <Button variant="primary" size="lg">
             Add Recipe
           </Button>
         </Link>
+      </div>
+      <div className="container">
+        <RecipeList />
       </div>
     </div>
   );
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,14 +1,19 @@
 import { connect } from "react-redux";
 import RecipeListItem from "./RecipeListItem";
+import SearchForm from "./SearchForm";
 
 export const RecipeList = (props) => {
   const filter = props.filters.text.toLowerCase();
   const filteredRecipes = props.recipes.filter(
-    (recipe) => recipe.name.toLowerCase().includes(filter) || recipe.ingredients.join().toLowerCase().includes(filter)
+    (recipe) =>
+      recipe.name.toLowerCase().includes(filter) ||
+      recipe.ingredients.join().toLowerCase().includes(filter)
   );
   return (
     <>
       <h1>{props.recipes.length === 0 ? "Start adding recipes!" : "Your saved recipes:"}</h1>
+      <SearchForm />
+
       <div className="recipes">
         {filteredRecipes.map((recipe) => (
           <RecipeListItem key={recipe.id} recipe={recipe} />
